37aa1bd56fe9a3ca4f5448f809732b6e7b68183f
Alix Ospino <alixhelena@yahoo.es>
2022-10-28 11:24:20 -0500

    endpoint permiso

---
+++ src/main/java/Repositorios/RepositorioPermiso.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package Repositorios;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import Modelos.Permiso;
+
+/**
+ * @author Usuario'Alix
+ *
+ */
+public interface RepositorioPermiso extends MongoRepository<Permiso,String>{
+
+}
